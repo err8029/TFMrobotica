5d31b657765edbaac3be5d50d74a5c9dc7150d20
Eric <err8029@gmail.com>
2018-07-01 09:25:58 +0200

    code reestructured, now move part is separated from GUI

--- display_image.m
+++ display_image.m
@@ -1,4 +1,16 @@
-function display_image(cam_msg,img_handle)
-    axes(img_handle)
-    imshow(img_view(cam_msg))
+function display_image()
+    %global vars for images
+    global img_sub
+    global img2_sub
+    global img2_handle
+    global img1_handle
+    %obtain the latest image from the subscriber
+    cam1_msg=img_sub.LatestMessage;
+    cam2_msg=img2_sub.LatestMessage;
+    %change to the axes of robot 1 and show the image
+    axes(img1_handle)
+    imshow(img_view(cam2_msg))
+    %change to the axes of robot 2 and show the image
+    axes(img2_handle)
+    imshow(img_view(cam1_msg))
 end
--- move_up1.m
+++
@@ -1,8 +0,0 @@
-function move_up1(obj,event,img_sub)
-    for c=1:1:100
-        send_velocity(0.5,0,1)
-    end
-    %unpack the messages
-    cam1_msg=img_sub.LatestMessage;
-    display_image(cam1_msg,img1_handle)
-end
