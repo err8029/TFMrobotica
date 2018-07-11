df8587441a827173946b5509c1c940cb36abd8ad
Eric <err8029@gmail.com>
2018-07-11 16:02:28 +0200

    fixed degrees in GUI

--- odom_management/odom_update.m
+++ odom_management/odom_update.m
@@ -8,7 +8,7 @@ function odom_update()
     odom_obtention();
     text=sprintf('%s\n%s\n%s\n%s','Position',['X: ' num2str(position1(1)) ' m'],['Y: ' num2str(position1(2)) ' m'],['Z: ' num2str(position1(3)) ' m']);
     position1_text.String=text;
-    text=sprintf('%s\n%s\n%s\n%s','Orientation',['X: ' num2str(orientation1(1)) ' deg'],['Y: ' num2str(orientation1(2)) ' deg'],['Z: ' num2str(orientation1(3)) ' deg']);
+    text=sprintf('%s\n%s\n%s\n%s','Orientation',['X: ' num2str((180/pi)*orientation1(1)) ' deg'],['Y: ' num2str(orientation1(2)) ' deg'],['Z: ' num2str(orientation1(3)) ' deg']);
     orientation1_text.String=text;
 end
 
