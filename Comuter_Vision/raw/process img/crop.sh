6be7cdb70a1362de0d86d55f17a145c0ac37bfa2
Eric <err8029@gmail.com>
2018-10-04 15:27:52 +0200

    new images added for tests and test efficiency is now coherent

--- Comuter_Vision/raw/process img/crop.sh
+++ Comuter_Vision/raw/process img/crop.sh
@@ -5,6 +5,8 @@
 mogrify -crop 900x900+340+100 *.png
 echo "crop done"
 mogrify -resize 3.555556% *.png
+#mogrify -resize 28.44445% *.png
+#for a 256x256 pic
 echo "resize done"
 #rename the files to 001.png style
 i=1
--- Comuter_Vision/raw/turtle_detector.m
+++ Comuter_Vision/raw/turtle_detector.m
@@ -1,16 +1,15 @@
 img_db=[];
 
+%extract the list of permutations of categories to train
 categories=csvread('categories.csv');
 
 num_cat=2;%number of categories
 %categories
 %1 - kitty
 %0 - nothing
-imds = imageDatastore('/home/eric/Escritorio/tfm_practico/Comuter_Vision/raw/turtle','Labels',categorical(categories)');
+imds = imageDatastore('./turtle32','Labels',categorical(categories)');
 
-% imds = imageDatastore(fullfile(rootFolder, categories), ...
-%     'LabelSource', 'foldernames');
-
+%%%%%%%%%%%%%design of the network%%%%%%%%%%%%%
 varSize = 32;
 conv1 = convolution2dLayer(5,varSize,'Padding',2,'BiasLearnRateFactor',2);
 conv1.Weights = gpuArray(single(randn([5 5 3 varSize])*0.0001));
@@ -35,37 +34,70 @@ layers = [
     reluLayer();
     fc2;
     softmaxLayer()
-    classificationLayer()];
+classificationLayer()];
 
+%%%%%%%%%%%%%training options%%%%%%%%%%%%%%%%%%%%
 opts = trainingOptions('sgdm', ...
     'InitialLearnRate', 0.01, ...
     'LearnRateSchedule', 'piecewise', ...
-    'LearnRateDropFactor', 0.5, ...
-    'LearnRateDropPeriod', 8, ...
+    'LearnRateDropFactor', 0.1, ... %amount that the learn rate drops
+    'LearnRateDropPeriod', 8, ... %num of epochs to drop the learn rate
     'L2Regularization', 0.004, ...
-    'MaxEpochs', 200, ...
-    'MiniBatchSize', 200, ...
+    'MaxEpochs', 400, ...
+    'MiniBatchSize', 16384, ...
     'Verbose', true);
 
 [net, info] = trainNetwork(imds, layers, opts);
 
+%%%%%%%%%%%read test data and classify%%%%%%%%%%%%%%%%
 categories_test=csvread('categories_test.csv');
-imds_test = imageDatastore('/home/eric/Escritorio/tfm_practico/Comuter_Vision/raw/test','Labels',categorical(categories_test)');
+imds_test = imageDatastore('./test','Labels',categorical(categories_test)');
 labels = classify(net, imds_test);
-num_img_test=2;
+
+%%%%%%%%%%%%%%%%%%%%%%test all%%%%%%%%%%%%%%%%%%%%%%%% 
+num_img_test=26;
+good=0;
+bad=0;
+
+figure(2)
+for img=1:num_img_test
+    im = imread(imds_test.Files{img});
+    subplot(13,2,img)
+    imshow(im);
+    if labels(img) == imds_test.Labels(img)
+        colorText = 'g'; 
+        good=good+1;
+    else
+        colorText = 'r';
+        bad=bad+1;
+    end
+    title(char(labels(img)),'Color',colorText);
+end
+figure(3);
+per_good=round(good/(good+bad)*10000)/100;%percentage of robot
+per_bad=round(bad/(good+bad)*10000)/100;%percentage of nothing
+c = categorical({['Good: ' num2str(per_good) ' %'],['Bad: ' num2str(per_bad) ' %']});
+bar(c,[good,bad])%draw graph
+title('Results of the test data');
+xlabel('Guessings');
+ylabel('Amount (u)');
+grid('on');
+
+%%%%%%%%random test%%%%%%%%%%%%%%%%%%%
+% num_img_test=2;
+% 
+% ii = randi(num_img_test);
+% im = imread(imds_test.Files{ii});
+% figure(3)
+% imshow(im);
+% if labels(ii) == imds_test.Labels(ii)
+% colorText = 'g'; 
+% else
+%  colorText = 'r';
+% end
+% title(char(labels(ii)),'Color',colorText);
  
- ii = randi(num_img_test);
- im = imread(imds_test.Files{ii});
- figure(2)
- imshow(im);
- if labels(ii) == imds_test.Labels(ii)
-    colorText = 'g'; 
- else
-     colorText = 'r';
- end
- title(char(labels(ii)),'Color',colorText);
- 
- %%%%%%%%%%%%categories distribution graph%%%%%%%%%
+%%%%%%%%%%%%%%categories distribution graph%%%%%%%%%%%%%
 robot=0;
 nothing=0;
 %collect the elements of each category
@@ -77,7 +109,10 @@ for cell=1:length(categories)
     end
 end
 fig=figure(1);
-c = categorical({'robot','nothing'});
+per_robot=round(robot/(robot+nothing)*10000)/100;%percentage of robot
+per_nothing=round(nothing/(robot+nothing)*10000)/100;%percentage of nothing
+
+c = categorical({['Robot: ' num2str(per_robot) ' %'],['Nothing: ' num2str(per_nothing) ' %']});
 bar(c,[robot,nothing])%draw graph
 title('Categories distribution');
 xlabel('categories');
