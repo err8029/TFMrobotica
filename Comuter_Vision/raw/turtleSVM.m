4ddd58b3d4536452b1e599ce2402e5522bda36e9
Eric <err8029@gmail.com>
2018-10-05 15:53:43 +0200

    new linear SVM added with a new real database, accuracy now is 100%

--- Comuter_Vision/raw/turtleSVM.m
+++ Comuter_Vision/raw/turtleSVM.m
@@ -1,13 +1,14 @@
 
 %Load two image categories
 categories=csvread('categories.csv');
-imds = imageDatastore('./turtle256','Labels',categorical(categories)');
+imds = imageDatastore('./turtle32','Labels',categorical(categories)');
 
 %Split the data set into a training and test data. Pick 30% of images from each set for the training data and the remainder 70% for the test data
-[trainingSet,testSet] = splitEachLabel(imds,0.3,'randomize');
+[trainingSet,testSet] = splitEachLabel(imds,0.7,'randomize');
 
 %Create bag of visual words
-bag = bagOfFeatures(trainingSet,'VocabularySize',50000);
+bag = bagOfFeatures(trainingSet,'VocabularySize',500,'StrongestFeatures',0.8,...
+    'BlockWidth',[32 64 96 128],'GridStep',[8 8]);
 
 %Train a classifier with the training sets
 categoryClassifier = trainImageCategoryClassifier(trainingSet,bag);
